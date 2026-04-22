9b486bb85839ae4290769189e941c6c2a3c341fc
Jakai F <jakai2111@gmail.com>
2026-04-22 14:59:08 -0400

    fixed backend formatting

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,5 +1,7 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
@@ -8,4 +10,4 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME || "Crosstunes",
 });
 
-module.exports = pool;
+export default pool;
