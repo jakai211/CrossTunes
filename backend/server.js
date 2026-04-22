aa40bcf4b2bdeaa9277b17e4e542aa936a63c093
Jakai F <jakai2111@gmail.com>
2026-04-22 15:51:15 -0400

    Helps me connect to the database and server?

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,28 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+
 import registerRoute from "./routes/register.js";
 import loginRoute from "./routes/login.js";
 
 dotenv.config();
 
 const app = express();
+
+// Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
 app.use("/register", registerRoute);
 app.use("/login", loginRoute);
 
-// Test route for networking
+// Test route
 app.get("/test", (req, res) => {
   res.json({ message: "Backend is running" });
 });
 
+// Start server
 app.listen(process.env.PORT, () => {
   console.log(`Backend running on port ${process.env.PORT}`);
 });
